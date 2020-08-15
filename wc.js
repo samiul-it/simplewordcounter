0dd327ae0fe4ea8285ef6eb1ea64315c6f241857
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2020-08-15 17:50:08 +0600

    Add files via upload

---
+++ wc.js
@@ -0,0 +1,12 @@
+alert("This is a wordcount Software")
+
+function collector ()
+{
+
+        var store= document.getElementById("textfield").value;
+       // document.write(store);
+        var print;
+        print=store.length;
+        alert("Total Number Of Words:"+print);
+
+}
